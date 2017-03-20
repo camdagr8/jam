1232b12f1aecd96b37d7fb7f6f200408dc1c9ba1
Cameron Tullos <ctullos@resourceammirati.com>
2017-03-20 16:18:42 -0400

    Fixed core.plugin function so that if a module directory is not present, it returns an empty array of plugins.

--- src/app/_core/prefs.js
+++ src/app/_core/prefs.js
@@ -30,34 +30,34 @@ module.exports = (req, res, next) => {
     jam['widgets']     = [];
 
     Parse.Cloud.run('config_get').then((result) => { // Get Config objects
-        log('config_get');
+
         let keys = _.keys(result);
         keys.forEach((key) => { jam[key] = result[key]; });
 
     }, (err) => { // Not able to get the configs
-        log('no configs');
 
         jam.installed = false;
         next();
 
     }).then(() => { // Get the current user from session token
-        log('get current user from session');
+
         let stoken = (req.cookies.hasOwnProperty(core.skey)) ? req.cookies[core.skey] : null;
         return (stoken) ? Parse.Cloud.run('user_session_get', {token: stoken}) : null;
 
     }).then((user) => { // Set the currentuser value: Parse.User || null
-        log('set current user');
+
         jam.currentuser = user;
 
     }, (err) => { // No current user: Keep going
+
         log(err);
 
     }).then(() => {
-        log('content_get_pages');
+
         return Parse.Cloud.run('content_get_pages');
 
     }).then((pages) => {
-        log('got pages');
+
         jam['pages'] = pages;
 
     }, (err) => {
@@ -66,7 +66,6 @@ module.exports = (req, res, next) => {
 
     }).then(() => {
 
-        log('helpers');
         // Core helpers & plugins
         jam['helpers'] = core.plugins(appdir + '/_core/helper', true);
         jam['plugins'] = core.plugins(appdir + '/_core/plugin');
@@ -74,9 +73,11 @@ module.exports = (req, res, next) => {
         // User helpers & plugins
         jam['helpers'] = jam.helpers.concat(core.plugins(appdir + '/helper', true));
         jam['plugins'] = jam.plugins.concat(core.plugins(appdir + '/plugin'));
+    }, (err) => {
+
+        log('helpers error');
 
     }).then(() => { // Done!
-        log(jam);
         next();
     });
 };
