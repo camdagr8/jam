d6d5e17c5939420d38d383c4d00d8732b71a14ec
Cameron Tullos <ctullos@resourceammirati.com>
2017-03-28 12:58:54 -0400

    Added user_set_pref cloud function.

--- src/app/_core/cloud/user.js
+++ src/app/_core/cloud/user.js
@@ -1,9 +1,13 @@
+const _ 		= require('underscore');
+
+
 /**
  *
  * TODO:
  * user_post
  * user_delete
  * user_set_role
+ * user_set_pref
  */
 
 /**
@@ -42,6 +46,57 @@ const users_get = (request, response, results = []) => {
 	});
 };
 
+const user_session_get = (request, response) => {
+
+    if (!request.params.hasOwnProperty('token')) { response.error({message: 'token is a required parameter'}); }
+
+    let qry = core.query({table: '_Session'});
+
+    let u = null;
+
+    qry.equalTo('sessionToken', request.params.token);
+    qry.include('user');
+    qry.first({useMasterKey: true}).then((session) => {
+        if (session) {
+            u = session.get('user');
+            return u;
+        } else {
+            response.success(null);
+        }
+    }, (err) => {
+
+        response.error(err);
+
+    }).then((user) => {
+
+        if (user.get('roles')) {
+            response.success(user);
+            qry.resolve(user);
+        } else {
+            let rqry = new Parse.Query(Parse.Role);
+            rqry.include('roles');
+            rqry.equalTo('users', user);
+
+            return rqry.find();
+        }
+
+    }).then((roles) => {
+        let r = {};
+
+        roles.forEach((role) => {
+            let n = String(role.get('name')).toLowerCase();
+            r[n] = {objectId: role.id, level: role.get('level')};
+        });
+
+        u.set('objectId', u.id);
+        u.set('roles', r);
+
+        return u.save(null, {useMasterKey: true});
+
+    }).then(() => {
+        response.success(u);
+    });
+};
 
 /**
  *
@@ -52,7 +107,7 @@ const users_get = (request, response, results = []) => {
  *
  * @description Gets users list starting at skip 0 - 1000.
  *
- * @param skip {Number} The starting index. Default: 0.
+ * @param request.params.skip {Number} The starting index. Default: 0.
  */
 Parse.Cloud.define('users_get', (request, response) => {
 
@@ -74,57 +129,76 @@ Parse.Cloud.define('users_get', (request, response) => {
  *
  * @description Gets a user via the req.cookies.session token
  *
- * @param token {String} The session token to query for.
+ * @param request.params.token {String} The session token to query for.
  */
 
-Parse.Cloud.define('user_session_get', (request, response) => {
+Parse.Cloud.define('user_session_get', user_session_get);
 
-	if (!request.params.hasOwnProperty('token')) { response.error({message: 'token is a required parameter'}); }
+/**
+ *
+ * user_pref_set
+ *
+ * @author Cam Tullos cam@tullos.ninja
+ * @since 1.0.0
+ *
+ * @description Sets the pref.property of the supplied user.
+ *
+ * @param request.user {Object} The user object to update.
+ * @param request.params.prefs {Object} The name value pairs to apply to the user. If the value is set to null, the pref property will be unset.
+ * @param request.params.token {String} The session ID of the user. This will call the user_session_get function and set the request.user value.
+ */
+const user_pref_set = (request, response) => {
 
-	let qry = core.query({table: '_Session'});
+    if (!request['user']) {
+        if (request.params.hasOwnProperty('token')) {
+            Parse.Cloud.run('user_session_get', request.params).then((user) => {
+                request['user'] = user;
+                user_pref_set(request, response);
+            }, (err) => {
+                response.error(err.message);
+            });
+        } else {
+            response.error('request.user is a required parameter');
+        }
+    } else {
+        let user     = request.user;
+        let keys     = _.keys(request.params.prefs);
+        let prefs    = user['pref'] || {};
 
-	let u = null;
+        keys.forEach((key) => {
+            let val = request.params.prefs[key];
+            if (val === null) {
+                delete prefs[key];
+            } else {
+                prefs[key] = val;
+            }
+        });
 
-	qry.equalTo('sessionToken', request.params.token);
-	qry.include('user');
-	qry.first({useMasterKey: true}).then((session) => {
-		if (session) {
-			u = session.get('user');
-			return u;
-		} else {
-			response.success(null);
-		}
-	}, (err) => {
+        user.set('prefs', prefs);
+        user.save(null, {useMasterKey: true}).then((user) => {
+            if (jam.currentuser !== null) {
+                if (jam.currentuser.id === user.id) {
+                    return jam.currentuser.fetch();
+                } else {
+                    return user;
+                }
+            } else {
+                return user;
+            }
+        }, (err) => {
+            response.error(err.message);
+        }).then((user) => {
+            response.success(user);
+        });
+    }
+};
 
-		response.error(err);
+Parse.Cloud.define('user_pref_set', (request, response) => {
 
-	}).then((user) => {
+    // No request.user? No request.token? Got jam.currentuser?
+    if (!request['user'] && !request.params['token'] && jam.currentuser !== null) {
+        request['user'] = jam.currentuser;
+    }
 
-		if (user.get('roles')) {
-			response.success(user);
-			qry.resolve(user);
-		} else {
-			let rqry = new Parse.Query(Parse.Role);
-				rqry.include('roles');
-				rqry.equalTo('users', user);
-
-			return rqry.find();
-		}
-
-	}).then((roles) => {
-		let r = {};
-
-		roles.forEach((role) => {
-			let n = String(role.get('name')).toLowerCase();
-			r[n] = {objectId: role.id, level: role.get('level')};
-		});
-
-		u.set('objectId', u.id);
-		u.set('roles', r);
-
-		return u.save(null, {useMasterKey: true});
-
-	}).then(() => {
-		response.success(u);
-	});
+    user_pref_set(request, response);
 });
